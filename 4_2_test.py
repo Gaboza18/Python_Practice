3c6623143761c3bfce1dc1fc10055a7e257d111c
Gaboza18 <91782552+Gaboza18@users.noreply.github.com>
2023-03-05 17:42:51 +0900

    first commit

---
+++ 4_2_test.py
@@ -0,0 +1,14 @@
+print('Hello World')
+print("Hello World")
+
+print('''
+Hello
+World
+''')
+
+print("'1'+'1'", '1'+'1')
+print('Hello World' * 1000)
+print("len('Hello World' * 1000) 문자길이: ",len('Hello World' * 1000))
+
+# 문자열 대체 -> replace.('원본', '대체할문자')
+print("'Hello World'.replace('World','universe')", 'Hello World'.replace('World','universe'))
---
+++ 4_3_list.py
@@ -0,0 +1,17 @@
+students = ["ssy", "sss", "zzzz"]
+grades = [4,1,2]
+
+print("students[1]", students[1])
+print("len(students)", len(students))
+
+print("min(grades)", min(grades))
+print("max(grades)", max(grades))
+print("sum(grades)", sum(grades))
+
+# 통계
+import statistics
+print("statistics.mean(grades)",statistics.mean(grades))
+
+# 랜덤
+import random
+print("random.choice(students)",random.choice(students))
